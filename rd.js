2faf92740aaf36a074aacd6b06ff4d37681a1859
Brosko123 <ytur123@tutamail.com>
2024-11-03 13:25:03 +0530

    Update rd.js

--- rd.js
+++ rd.js
@@ -1,1 +1,1 @@
-
+"use strict";export default function rd(req,res){if(res.headersSent)return;res.setHeader("content-length",0);res.removeHeader("cache-control");res.removeHeader("expires");res.removeHeader("date");res.removeHeader("etag");res.setHeader("location",encodeURI(req.params.url));res.status(302).end()}
