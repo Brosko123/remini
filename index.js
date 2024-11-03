fbac2e12acab856da798df56f72426e2bab7ea5c
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-03 11:06:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
 #!/usr/bin/env node
-"use strict";
-
-import express from 'express';
-import params from './src/params.js';
-import proxy from './src/proxy.js';
-
-const app = express();
-
-// Uncomment the next line if you want to trust the proxy
-// app.enable('trust proxy');
-
-app.get('/', params, proxy);
-app.get('/favicon.ico', (req, res) => res.status(204).end());
-
-export default app;
+"use strict";import r from"express";import s from"./src/params.js";import t from"./src/proxy.js";const o=r();o.get("/",s,t);o.get("/favicon.ico",(r,s)=>s.status(204).end());export default o;
