6a2edc2a0351bed1011deea7e6396aa71589f61e
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-11-03 11:07:20 +0530

    Update server1.js

--- server1.js
+++ server1.js
@@ -1,5 +1,2 @@
-import app from './index.js'; // Import the app from index.js
-import http from 'http'; // Import the http module
-
-const server = http.createServer(app);
-server.listen(process.env.PORT || 8080);
+#!/usr/bin/env node
+"use strict";import s from"express";import o from"./src/params.js";import r from"./src/proxy.js";const t=process.env.PORT||8080;const e=s();e.get("/",o,r);e.get("/favicon.ico",(s,o)=>o.status(204).end());e.listen(t,()=>console.log(`Listening on ${t}`));
