e5b236b9bbae2bdf1792e1295bcbe85500a6891e
Brosko123 <ytur123@tutamail.com>
2024-11-04 12:42:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-"use strict";import helmet from "helmet";import express from"express";import pp from"./pp.js";import po from"./po.js";const PORT=process.env.PORT||8080;const app=express();app.use(helmet());app.disable('x-powered-by');app.get("/",pp,po);app.get("/favicon.ico",(req,res)=>res.status(204).end());app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
+"use strict";import helmet from "helmet";import express from"express";import pp from"./pp.js";import po from"./po1.js";const PORT=process.env.PORT||8080;const app=express();app.use(helmet());app.disable('x-powered-by');app.get("/",pp,po);app.get("/favicon.ico",(req,res)=>res.status(204).end());app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
